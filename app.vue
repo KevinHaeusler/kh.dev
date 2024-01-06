ad238550290b9e804c26763d8e58b74abbda324d
KevinHaeusler <kevin.haeusler@khdev.ch>
2024-01-06 11:04:45 +0100

    Added nuxt-tailwindcss, nuxt-color-mode and implemented darkmode

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup>
+const colorMode = useColorMode()
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-red-500 dark:bg-blue-500">
+    test
+    <p class="p-4 pb-2">
+      <select
+        v-model="colorMode.preference"
+        class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
+      >
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </p>
   </div>
 </template>
